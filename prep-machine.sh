8bc989c78bb4f7256c7f5f2b753e898a2f39a73d
Samuel Anudeep <anudeeb@microsoft.com>
2018-09-07 17:41:27 +0530

    changing location

--- prep-machine.sh
+++ prep-machine.sh
@@ -1,5 +1,5 @@
-mkdir -p /etc/azure
-cd /etc/azure
+mkdir -p /etc/azure/sam
+cd /etc/azure/sam
 
 wget https://raw.githubusercontent.com/dragonfly91/AzureBackupIgnite2018/1c810c2df489fd3ef589ae2e90aa72a286ca896c/mysql-app-consistent-backup-env.sh
 wget https://raw.githubusercontent.com/dragonfly91/AzureBackupIgnite2018/1c810c2df489fd3ef589ae2e90aa72a286ca896c/mysql-app-consistent-backup-funcs.sh
@@ -9,4 +9,4 @@ wget https://raw.githubusercontent.com/dragonfly91/AzureBackupIgnite2018/1c810c2
 
 wget https://raw.githubusercontent.com/MicrosoftAzureBackup/VMSnapshotPluginConfig/6ee4794d92870f452143cd987299929dadb8f012/VMSnapshotScriptPluginConfig.json
 
-sed -i 's#"preScriptLocation" : ""#"preScriptLocation" : "/etc/azure/mysql-app-consistent-backup-env.sh"#g' VMSnapshotScriptPluginConfig.json
+sed -i 's#"preScriptLocation" : ""#"preScriptLocation" : "/etc/azure/sam/mysql-app-consistent-backup-env.sh"#g' VMSnapshotScriptPluginConfig.json
