a800e3d5f743f54b0c193633755384c5a1cac98a
Samuel Anudeep <anudeeb@microsoft.com>
2018-09-07 18:46:14 +0530

    sed fix

--- prep-machine.sh
+++ prep-machine.sh
@@ -8,7 +8,8 @@ wget -q https://raw.githubusercontent.com/dragonfly91/AzureBackupIgnite2018/1c81
 wget -q https://raw.githubusercontent.com/dragonfly91/AzureBackupIgnite2018/1c810c2df489fd3ef589ae2e90aa72a286ca896c/post-mysql-backup.sh -O $SCRIPT_LOCATION/post-mysql-backup.sh
 wget -q https://raw.githubusercontent.com/MicrosoftAzureBackup/VMSnapshotPluginConfig/6ee4794d92870f452143cd987299929dadb8f012/VMSnapshotScriptPluginConfig.json -O $SCRIPT_LOCATION/VMSnapshotScriptPluginConfig.json
 
-sed -i 's#"preScriptLocation" : ""#"preScriptLocation" : "$SCRIPT_LOCATION/mysql-app-consistent-backup-env.sh"#g' $SCRIPT_LOCATION/VMSnapshotScriptPluginConfig.json
+sed -i 's#"preScriptLocation" : ""#"preScriptLocation" : "'"$SCRIPT_LOCATION"'/pre-mysql-backup.sh"#g' $SCRIPT_LOCATION/VMSnapshotScriptPluginConfig.json
+sed -i 's#"preScriptLocation" : ""#"postScriptLocation" : "'"$SCRIPT_LOCATION"'/post-mysql-backup.sh"#g' $SCRIPT_LOCATION/VMSnapshotScriptPluginConfig.json
 
 chmod 700 $SCRIPT_LOCATION/pre-mysql-backup.sh
 chmod 700 $SCRIPT_LOCATION/post-mysql-backup.sh
