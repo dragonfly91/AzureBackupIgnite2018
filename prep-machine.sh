985214bd9d97ab07229425e219f4c737103a70e9
Samuel Anudeep <anudeeb@microsoft.com>
2018-09-07 18:21:59 +0530

    Wget overwrites

--- prep-machine.sh
+++ prep-machine.sh
@@ -1,12 +1,14 @@
-mkdir -p /etc/azure/sam
-cd /etc/azure/sam
+SCRIPT_LOCATION=/etc/azure/sam
 
-wget https://raw.githubusercontent.com/dragonfly91/AzureBackupIgnite2018/1c810c2df489fd3ef589ae2e90aa72a286ca896c/mysql-app-consistent-backup-env.sh
-wget https://raw.githubusercontent.com/dragonfly91/AzureBackupIgnite2018/1c810c2df489fd3ef589ae2e90aa72a286ca896c/mysql-app-consistent-backup-funcs.sh
-wget https://raw.githubusercontent.com/dragonfly91/AzureBackupIgnite2018/1c810c2df489fd3ef589ae2e90aa72a286ca896c/mysql-app-consistent-backup.sh
-wget https://raw.githubusercontent.com/dragonfly91/AzureBackupIgnite2018/1c810c2df489fd3ef589ae2e90aa72a286ca896c/pre-mysql-backup.sh
-wget https://raw.githubusercontent.com/dragonfly91/AzureBackupIgnite2018/1c810c2df489fd3ef589ae2e90aa72a286ca896c/post-mysql-backup.sh
+wget -q https://raw.githubusercontent.com/dragonfly91/AzureBackupIgnite2018/1c810c2df489fd3ef589ae2e90aa72a286ca896c/mysql-app-consistent-backup-env.sh -O $SCRIPT_LOCATION/mysql-app-consistent-backup-env.sh
+wget -q https://raw.githubusercontent.com/dragonfly91/AzureBackupIgnite2018/1c810c2df489fd3ef589ae2e90aa72a286ca896c/mysql-app-consistent-backup-funcs.sh -O $SCRIPT_LOCATION/mysql-app-consistent-backup-funcs.sh
+wget -q https://raw.githubusercontent.com/dragonfly91/AzureBackupIgnite2018/1c810c2df489fd3ef589ae2e90aa72a286ca896c/mysql-app-consistent-backup.sh -O $SCRIPT_LOCATION/mysql-app-consistent-backup.sh
+wget -q https://raw.githubusercontent.com/dragonfly91/AzureBackupIgnite2018/1c810c2df489fd3ef589ae2e90aa72a286ca896c/pre-mysql-backup.sh -O $SCRIPT_LOCATION/pre-mysql-backup.sh
+wget -q https://raw.githubusercontent.com/dragonfly91/AzureBackupIgnite2018/1c810c2df489fd3ef589ae2e90aa72a286ca896c/post-mysql-backup.sh -O $SCRIPT_LOCATION/post-mysql-backup.sh
+wget -q https://raw.githubusercontent.com/MicrosoftAzureBackup/VMSnapshotPluginConfig/6ee4794d92870f452143cd987299929dadb8f012/VMSnapshotScriptPluginConfig.json -O $SCRIPT_LOCATION/VMSnapshotScriptPluginConfig.json
 
-wget https://raw.githubusercontent.com/MicrosoftAzureBackup/VMSnapshotPluginConfig/6ee4794d92870f452143cd987299929dadb8f012/VMSnapshotScriptPluginConfig.json
+sed -i 's#"preScriptLocation" : ""#"preScriptLocation" : "$SCRIPT_LOCATION/mysql-app-consistent-backup-env.sh"#g' VMSnapshotScriptPluginConfig.json
 
-sed -i 's#"preScriptLocation" : ""#"preScriptLocation" : "/etc/azure/sam/mysql-app-consistent-backup-env.sh"#g' VMSnapshotScriptPluginConfig.json
+chmod 700 $SCRIPT_LOCATION/pre-mysql-backup.sh
+chmod 700 $SCRIPT_LOCATION/post-mysql-backup.sh
+chmod 600 $SCRIPT_LOCATION/VMSnapshotScriptPluginConfig.json
